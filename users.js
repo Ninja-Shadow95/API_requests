c323c613fffc1ca0bdac1c908143e66442a28902
Ninja-Shadow95 <150259654+Ninja-Shadow95@users.noreply.github.com>
2024-02-16 16:38:43 +0530

    Update users.js

--- users.js
+++ users.js
@@ -1,20 +1,20 @@
 users = [
     {
         id: 1,
-        name: 'P Sai Kiran',
-        email: 'psaikiran@gmail.com',
+        name: 'Ninjaaaa',
+        email: 'ninjaaa@gmail.com',
         type: 'admin'
     },
     {
     id: 2,
-    name: 'Srinivas',
-    email: 'vasu@gmail.com',
+    name: 'Shadooooo',
+    email: 'Shadoooooo@gmail.com',
     status: 'guest'
     },
     {
     id: 3,
-    name: 'Thrishank',
-    email: 'thrishank@gmail.com',
+    name: 'anonymous',
+    email: 'anonymous@gmail.com',
     status: 'admin'
     }
 ];
